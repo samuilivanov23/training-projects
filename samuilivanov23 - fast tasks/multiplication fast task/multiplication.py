14c70dd1b92283ecd252a52cbcff6d2716c01c79
Samuil Ivanov <samuil2001ivanov@gmail.com>
2020-08-11 11:45:55 +0300

    Removing debug lines

--- samuilivanov23 - fast tasks/multiplication fast task/multiplication.py
+++ samuilivanov23 - fast tasks/multiplication fast task/multiplication.py
@@ -1,7 +1,6 @@
 import math
 
 input_number = int(input("Enter a number between 0 and  3 200 000: "))
-print(input_number)
 
 quadratics_string = ""
 
@@ -9,5 +8,4 @@ for i in range(1, input_number + 1):
     quadratic_number = int(math.pow(i, 2))
     quadratics_string += str(quadratic_number)
 
-print(quadratics_string)
 print(quadratics_string[input_number - 1])
