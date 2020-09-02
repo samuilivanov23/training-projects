dc15aa1c00b185801bf5366aa6b5c2f03142bd2e
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-09-02 15:53:09 +0300

    Fixing fractions fast task

--- samuilivanov23-fast_tasks/fractions-fast-task/fractions.py
+++ samuilivanov23-fast_tasks/fractions-fast-task/fractions.py
@@ -1,3 +1,11 @@
+from operator import attrgetter
+
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.numerator = numerator
+        self.denominator = denominator
+        self.fraction = numerator/denominator
+
 z = int(input("z: "))
 m = int(input("m: "))
 n = int(input("n: "))
@@ -8,15 +16,34 @@ while (z < 1 or z > 50000000) or (m < 1 or m > 100000) or (n < 1 or n > 100000):
     m = int(input("m: "))
     n = int(input("n: "))
 
-p, q = m, n
+# p, q = m, n
+p, q = 1, 2
 
-while q <= z:
+
+fractions = [Fraction(1,2)]
+
+while q < z:
     if p == q:
         q += 1
-        p = 1
+        p = 2
+        fraction = Fraction(1, q)
+        fractions.append(fraction)
     if (not q%p == 0) and (p/q > m/n):
-        break
+        #min_fraction = min(fractions, key=attrgetter('fraction'))
+        fractions.append(Fraction(p, q))
     p+=1
+
+
+fractions.sort(key=lambda x: x.fraction)
+
+for fraction in fractions:
+    if fraction.fraction > m/n:
+        p = fraction.numerator
+        q = fraction.denominator
+        break
+
+
+#print("result: " + str(fractions[0].fraction))
 
 print("p: " + str(p))
 print("q: " + str(q))
