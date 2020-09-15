94a1862acd2f7af6f4092cd83562c28be5703709
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-09-15 20:23:37 +0300

    Adding additional diagram when filtering data by authors

--- samuilivanov23-chitanka/web_interface/interface.php
+++ samuilivanov23-chitanka/web_interface/interface.php
@@ -14,7 +14,6 @@
 
             $input_author = $_POST['author'];
             $input_book = $_POST['book'];
-            $input_word = $_POST['word'];
 
             $author_words_count;
             $author_longest_sentence;
@@ -22,10 +21,11 @@
             $book_longest_sentence;
             $word_in_books_occurs;
             $authors_rank_list = array();
+            $books_rank_list = array();
             $words_rank_list = array();
             $output_case = 0;
 
-            if( $input_author != "" || $input_book != "" || $input_word != "")
+            if( $input_author != "" || $input_book != "")
             {                
                 $dbConnection = new PDO('pgsql:host='. $dbhost_ . ';dbname=' . $dbname_, $dbuser_, $dbpass_) or die("Connection failed");
                 
@@ -33,30 +33,7 @@
                 $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $params = array();
 
-                if($input_word != "")
-                {
-                    $output_case = 3;
-
-                    $word_occurs_query = 'select distinct a.name as author_name, b.name as book_name from public."Authors" as a 
-                                          join public."Books" as b on a.id=b.author_id 
-                                          join public."Books_Words" as bw on b.id=bw.book_id 
-                                          join public."Words" as w on bw.word_id=w.id 
-                                          where w.word=:input_word order by a.name asc;';
-
-                    $params["input_word"] = $input_word;
-                    
-                    //get words occurrances
-                    $statement = $dbConnection->prepare($word_occurs_query);
-                    $statement->execute($params);
-                    $result = $statement->fetchAll();
-
-                    if(count($result) <= 0)
-                    {
-                        echo "<p>The input data is not matching any records</p><br>";
-                    }
-                    
-                }
-                else if($input_book != "")
+                if($input_book != "")
                 {                    
                     $output_case = 2;
 
@@ -84,53 +61,71 @@
                 {
                     $output_case = 1;
 
-                    $author_words_count_query = 'select count(distinct w.word) from public."Authors" as a 
-                                                join public."Books" as b on a.id=b.author_id
-                                                join public."Books_Words" as bw on b.id=bw.book_id
-                                                join public."Words" as w on bw.word_id=w.id where a.name=:input_author';
+                    // $author_words_count_query = 'select count(distinct w.word) from public."Authors" as a 
+                    //                             join public."Books" as b on a.id=b.author_id
+                    //                             join public."Books_Words" as bw on b.id=bw.book_id
+                    //                             join public."Words" as w on bw.word_id=w.id where a.name=:input_author';
                     
-                    $author_longest_sentence_query = 'select max(s.words_count) from public."Authors" as a 
-                                                    join public."Books" as b on a.id=b.author_id
-                                                    join public."Sentences" as s on b.id=s.book_id
-                                                    where a.name=:input_author';
+                    // $author_longest_sentence_query = 'select max(s.words_count) from public."Authors" as a 
+                    //                                 join public."Books" as b on a.id=b.author_id
+                    //                                 join public."Sentences" as s on b.id=s.book_id
+                    //                                 where a.name=:input_author';
 
-                    $author_books_query = 'select a.name as author_name, b.name as book_name from public."Authors" as a
-                                        join public."Books" as b on a.id=b.author_id
-                                        where a.name=:input_author';
+                    // $author_books_query = 'select a.name as author_name, b.name as book_name from public."Authors" as a
+                    //                     join public."Books" as b on a.id=b.author_id
+                    //                     where a.name=:input_author';
                     
-                    $params["input_author"] = $input_author;
+                    // $params["input_author"] = $input_author;
 
+                    // //get author's unique words count
+                    // $result = $dbConnection->prepare($author_words_count_query); 
+                    // $result->execute($params); 
+                    // $author_words_count = $result->fetchColumn();
 
-                    //get author's unique words count
-                    $result = $dbConnection->prepare($author_words_count_query); 
-                    $result->execute($params); 
-                    $author_words_count = $result->fetchColumn();
+                    // //get author's longest sentence
+                    // $result = $dbConnection->prepare($author_longest_sentence_query); 
+                    // $result->execute($params); 
+                    // $author_longest_sentence = $result->fetchColumn(); 
 
-                    //get author's longest sentence
-                    $result = $dbConnection->prepare($author_longest_sentence_query); 
-                    $result->execute($params); 
-                    $author_longest_sentence = $result->fetchColumn(); 
+                    // //get all author's books
+                    // $statement = $dbConnection->prepare($author_books_query);
+                    // $statement->execute($params);
+                    // $result = $statement->fetchAll();
 
-                    //get all author's books
-                    $statement = $dbConnection->prepare($author_books_query);
+                    // if(count($result) <= 0)
+                    // {
+                    //     echo "<p>The input data is not matching any records</p><br>";
+                    // }
+
+                    $sql_books_chart = 'select b.name, count(distinct w.word) as word_count from public."Authors" as a 
+                                        join public."Books" as b on a.id=b.author_id 
+                                        join public."Books_Words" as bw on b.id=bw.book_id 
+                                        join public."Words" as w on bw.word_id=w.id 
+                                        where a.name=:input_author
+                                        group by b.name order by word_count desc 
+                                        limit 10';
+                    
+                    $statement = $dbConnection->prepare($sql_books_chart);
                     $statement->execute($params);
-                    $result = $statement->fetchAll();
+                    $books_result = $statement->fetchAll();
 
-                    if(count($result) <= 0)
+                    foreach($books_result as $row)
                     {
-                        echo "<p>The input data is not matching any records</p><br>";
+                        $books_rank_list[] = $row["name"];
+                        $words_rank_list[] = $row["word_count"];
                     }
                 }
             }
             else
             {
+                $output_case = 3;
                 $sql = 'select a.name, count(distinct w.word) as word_count from public."Authors" as a 
-                        join public."Books" as b on a.id=b.author_id 
-                        join public."Books_Words" as bw on b.id=bw.book_id 
-                        join public."Words" as w on bw.word_id=w.id 
-                        group by a.name order by word_count desc 
-                        limit 10';
-                
+                join public."Books" as b on a.id=b.author_id 
+                join public."Books_Words" as bw on b.id=bw.book_id 
+                join public."Words" as w on bw.word_id=w.id 
+                group by a.name order by word_count desc 
+                limit 10';
+        
                 $connection_string = "host=" . $dbhost_ . " port=5432 dbname=" . $dbname_ . " user=" . $dbuser_ . " password=" . $dbpass_;
                 $dbConnection = pg_connect($connection_string);
 
@@ -146,11 +141,7 @@
                     $authors_rank_list[] = $row["name"];
                     $words_rank_list[] = $row["word_count"];
                 }
-
-                $output_case = 4;
             }
-
-            echo "<p> " . $output_case . " </p>";
         ?>
 
         <form name="Form" class="margin_style" action="" method="post" accept-charset="UTF-8">
@@ -166,27 +157,52 @@
                             value="<?php if (isset($_POST["book"])) echo $_POST["book"]; ?>"><br><br>
             </div>
 
-            <div style="width:300px">
-                <label for="word">Word</label><br>
-                <input type="text" id="word" name="word"
-                            value="<?php if (isset($_POST["word"])) echo $_POST["word"]; ?>"><br><br>
-            </div>
-
             <input class="button button_style" type="submit" value="Submit" name="Submit"> <br><br>
         </form>
 
         <div id="chart">
         </div>
 
-        <?php if ($output_case == 4) : ?>
+        <?php if ($output_case == 1) : ?>
+            <script type="text/javascript">
+                var books_rank_list =<?php echo json_encode($books_rank_list); ?>;
+                var words_rank_list =<?php echo json_encode($words_rank_list); ?>;
+
+                var books_words_chart = {
+                    type: 'bar',
+                    name: 'Unique words per author',
+                    x: books_rank_list,
+                    y: words_rank_list,
+                    marker: {
+                        color: '#C8A2C8',
+                        line: {
+                            width: 2.5
+                        },
+                    }
+                };
+
+                var data = [ books_words_chart ];
+
+                var layout = { 
+                    title: 'Top 10 books rank list',
+                    font: {size: 14},
+                    margin: {
+                        b: 150,
+                    },    
+                };
+
+                var config = {responsive: true}
+
+                Plotly.newPlot('chart', data, layout, config );
+            </script>
+        <?php endif; ?>
+
+        <?php if ($output_case == 3) : ?>
             <script type="text/javascript">
                 var authors_rank_list =<?php echo json_encode($authors_rank_list); ?>;
                 var words_rank_list =<?php echo json_encode($words_rank_list); ?>;
 
-                console.log(authors_rank_list);
-                console.log(words_rank_list);
-
-                var unique_words_chart = {
+                var authors_words_chart = {
                     type: 'bar',
                     name: 'Unique words per author',
                     x: authors_rank_list,
@@ -199,11 +215,14 @@
                     }
                 };
 
-                var data = [ unique_words_chart ];
+                var data = [ authors_words_chart ];
 
                 var layout = { 
-                    title: 'Testing chart',
+                    title: 'Top 10 authors rank list',
                     font: {size: 14},
+                    margin: {
+                        b: 150,
+                    }, 
                 };
 
                 var config = {responsive: true}
@@ -212,53 +231,12 @@
             </script>
         <?php endif; ?>
 
-        <?php if($output_case == 1) : ?>
-            <p> Брой уникални думи в творчеството му: <?php echo htmlspecialchars($author_words_count) ?></p>
-            <p> Най-дългото му изречение е с <?php echo htmlspecialchars($author_longest_sentence) ?> думи</p>
-
-            <table id="customers">
-                <thead>
-                    <tr>
-                        <th>Име на автор</th>
-                        <th>Име на книга</th>
-                    </tr>
-                </thead>
-                <tbody> 
-                    <?php foreach($result as $row) : ?>
-                        <tr>
-                            <td><?php echo htmlspecialchars($row["author_name"]) ?></td>
-                            <td><?php echo htmlspecialchars($row["book_name"]) ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        <?php endif; ?>
-
         <?php if($output_case == 2) : ?>
             <p> Брой уникални думи в книгата: : <?php echo htmlspecialchars($book_words_count) ?></p>
             <p> Най-дългото изречение в книгата е с <?php echo htmlspecialchars($book_longest_sentence) ?> думи</p>
         <?php endif; ?>
 
-        <?php if($output_case == 3) : ?>
-            <table id="customers">
-                <thead>
-                    <tr>
-                        <th>Име на автор</th>
-                        <th>Име на книга</th>
-                    </tr>
-                </thead>
-                <tbody> 
-                    <?php foreach($result as $row) : ?>
-                        <tr>
-                            <td><?php echo htmlspecialchars($row["author_name"]) ?></td>
-                            <td><?php echo htmlspecialchars($row["book_name"]) ?></td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
-        <?php endif; ?>
-
-        <?php if ($output_case == 4) : ?>
+        <?php if ($output_case == 3) : ?>
             <p>Не е извършено филтриране все още.</p>
         <?php endif; ?>
     </body>
