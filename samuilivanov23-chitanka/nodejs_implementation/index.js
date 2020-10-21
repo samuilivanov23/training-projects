beeb5f545e8ae85148ed6468203668bb99d8f775
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-10-22 01:12:56 +0300

    Adding autocomplete for the filters and fixing x data for the sentences diagram to be sorted (0-5 words, 5-10 words .... 20-70 words)

--- samuilivanov23-chitanka/nodejs_implementation/index.js
+++ samuilivanov23-chitanka/nodejs_implementation/index.js
@@ -4,7 +4,7 @@ const { Client } = require('pg');
 const data = require('./db_config');
 const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser');
-const { request, response } = require('express');
+const { request, response, query } = require('express');
 const port = 8000;
 
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}))
@@ -38,96 +38,201 @@ app.get('/', function(request, response) {
                 x_axis_items.push(book['name'])
                 y_axis_items.push(book['words_count'])
             })
+
+            let authors_autocomplete = []
+            query = `select name from authors`
+            client.query(query, (err, res) => {
+                if(err != null)
+                {
+                    console.log(err)
+                }
+                else
+                {
+                    res['rows'].forEach(author => {
+                        authors_autocomplete.push(author["name"])
+                    })
+                }
+            })
+
+            let books_autocomplete = []
+            query = `select name from books`
+            client.query(query, (err, res) => {
+                if(err != null)
+                {
+                    console.log(err)
+                }
+                else
+                {
+                    res['rows'].forEach(book => {
+                        books_autocomplete.push(book["name"])
+                    })
+                }
+            })
             
             let plotData = [x_axis_items, y_axis_items]
 
             response.render('home', {
-                plotData
+                plotData, authors_autocomplete, books_autocomplete
             })  
         }
     })
 });
 
-app.post('/author', function(request, response) {
+app.post('/plot', function(request, response) {
     let author_name = request.body.author
-
-    query = `select b.name, b.words_count from authors as a 
-               join books as b on a.id=b.author_id 
-               where a.name=$1 order by b.words_count desc limit 10`
-
-    let parameters = [author_name]
-    client.query(query, parameters, (err, res) => {
-        if(err != null)
-        {
-            console.log(err)
-        }
-        else
-        {
-            let x_axis_items = []
-            let y_axis_items = []
-            res['rows'].forEach(book => {
-                x_axis_items.push(book['name'])
-                y_axis_items.push(book['words_count'])
-            })
-            
-            let plotData = [x_axis_items, y_axis_items]
-
-            response.render('books-plot', {
-                plotData
-            })
-        }
-    })
-});
-
-app.post('/book', function(request, response) {
     let book_name = request.body.book
+    let query = ""
 
-    query = `select '0 - 5 words' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on
-        b.id=s.book_id where b.name=$1 and s.words_count>=$2 and s.words_count<$3 group by s.words_count) as a 
-        union
-        select '5 - 10 words' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$4 and s.words_count>=$5 and s.words_count<$6 group by s.words_count) as a
-        union
-        select '10 - 15 words' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$7 and s.words_count>=$8 and s.words_count<$9 group by s.words_count) as a
-        union
-        select '15 - 20 words' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$10 and s.words_count>=$11 and s.words_count<$12 group by s.words_count) as a                                                                   
-        union
-        select '20 - 70 words' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$13 and s.words_count>=$14 and s.words_count<$15 group by s.words_count) as a`    
+    if (book_name != "")
+    {
+        let book_name = request.body.book
+
+        query = `select '1' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on
+            b.id=s.book_id where b.name=$1 and s.words_count>=$2 and s.words_count<$3 group by s.words_count) as a 
+            union
+            select '2' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$4 and s.words_count>=$5 and s.words_count<$6 group by s.words_count) as a
+            union
+            select '3' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$7 and s.words_count>=$8 and s.words_count<$9 group by s.words_count) as a
+            union
+            select '4' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$10 and s.words_count>=$11 and s.words_count<$12 group by s.words_count) as a                                                                   
+            union
+            select '5' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$13 and s.words_count>=$14 and s.words_count<$15 group by s.words_count) as a
+            order by range asc`
+        
+        let first_start = '0';
+        let first_end = '5';
+        let second_end = '10';
+        let third_end = '15';
+        let fourth_end = '20';
+        let fifth_end = '70';
     
-    let first_start = '0';
-    let first_end = '5';
-    let second_end = '10';
-    let third_end = '15';
-    let fourth_end = '20';
-    let fifth_end = '70';
-
-    let parameters = [book_name, first_start, first_end, 
-                      book_name, first_end, second_end, 
-                      book_name, second_end, third_end, 
-                      book_name, third_end, fourth_end, 
-                      book_name, fourth_end, fifth_end]
+        let parameters = [book_name, first_start, first_end, 
+                          book_name, first_end, second_end, 
+                          book_name, second_end, third_end, 
+                          book_name, third_end, fourth_end, 
+                          book_name, fourth_end, fifth_end]
+        
+        let ranges_map = {
+            "1": "0 - 5 words",
+            "2": "5 - 10 words",
+            "3": "10 - 15 words",
+            "4": "15 - 20 words",
+            "5": "20 - 70 words"
+        }
     
-    client.query(query, parameters, (err, res) => {
-        if(err != null)
-        {
-            console.log(err)
-        }
-        else
-        {
-            let x_axis_items = []
-            let y_axis_items = []
+        client.query(query, parameters, (err, res) => {
+            if(err != null)
+            {
+                console.log(err)
+            }
+            else
+            {
+                let x_axis_items = []
+                let y_axis_items = []
+    
+                res['rows'].forEach(sentences => {
+                    x_axis_items.push(ranges_map[sentences['range']])
+                    y_axis_items.push(sentences['sum'])
+                })
 
-            res['rows'].forEach(sentences => {
-                x_axis_items.push(sentences['range'])
-                y_axis_items.push(sentences['sum'])
-            })
-            
-            let plotData = [x_axis_items, y_axis_items]
+                let authors_autocomplete = []
+                query = `select name from authors`
+                client.query(query, (err, res) => {
+                    if(err != null)
+                    {
+                        console.log(err)
+                    }
+                    else
+                    {
+                        res['rows'].forEach(author => {
+                            authors_autocomplete.push(author["name"])
+                        })
+                    }
+                })
 
-            response.render('sentences-plot', {
-                plotData
-            })
-        }
-    })
+                let books_autocomplete = []
+                query = `select name from books`
+                client.query(query, (err, res) => {
+                    if(err != null)
+                    {
+                        console.log(err)
+                    }
+                    else
+                    {
+                        res['rows'].forEach(book => {
+                            books_autocomplete.push(book["name"])
+                        })
+                    }
+                })
+                
+                let plotData = [x_axis_items, y_axis_items]
+    
+                response.render('sentences-plot', {
+                    plotData, authors_autocomplete, books_autocomplete
+                })
+            }
+        })   
+    }
+    else if(author_name != "")
+    {
+        query = `select b.name, b.words_count from authors as a 
+        join books as b on a.id=b.author_id 
+        where a.name=$1 order by b.words_count desc limit 10`
+
+        let parameters = [author_name]
+        client.query(query, parameters, (err, res) => {
+            if(err != null)
+            {
+                console.log(err)
+            }
+            else
+            {
+                let x_axis_items = []
+                let y_axis_items = []
+
+                res['rows'].forEach(book => {
+                    x_axis_items.push(book['name'])
+                    y_axis_items.push(book['words_count'])
+                })
+                
+                let authors_autocomplete = []
+                query = `select name from authors`
+                client.query(query, (err, res) => {
+                    if(err != null)
+                    {
+                        console.log(err)
+                    }
+                    else
+                    {
+                        res['rows'].forEach(author => {
+                            authors_autocomplete.push(author["name"])
+                        })
+                    }
+                })
+
+                let books_autocomplete = []
+                query = `select name from books`
+                client.query(query, (err, res) => {
+                    if(err != null)
+                    {
+                        console.log(err)
+                    }
+                    else
+                    {
+                        res['rows'].forEach(book => {
+                            books_autocomplete.push(book["name"])
+                        })
+                    }
+                })
+                
+                let plotData = [x_axis_items, y_axis_items]
+
+                response.render('books-plot', {
+                    plotData, authors_autocomplete, books_autocomplete
+                })
+            }
+        })
+    }
 });
 
 app.listen(port, () => {
---
+++ samuilivanov23-chitanka/nodejs_implementation/public/autocomplete.js
@@ -0,0 +1,151 @@
+function clearFilters()
+{
+    document.forms["Form"]["city"].value = "";
+    document.forms["Form"]["state"].value = "";
+    document.forms["Form"]["zip_code"].value = "";
+    document.forms["Form"]["first_point_latitude"].value = "";
+    document.forms["Form"]["first_point_longitude"].value = "";
+    document.forms["Form"]["second_point_latitude"].value = "";
+    document.forms["Form"]["second_point_longitude"].value = "";
+}
+
+//first arg -> the text field
+//second arg -> the array of cities
+function autocomplete(input, cities) 
+{
+    //used to track which is the active city we are about to choose
+    var currentFocus;
+    
+    //executed when someone writes in the text field
+    input.addEventListener("input", function(e) 
+    {
+        var all_items_div, single_item_div, i, val = this.value;
+        //close any already open lists of autocompleted values
+        closeAllLists();
+
+        if (!val) 
+        {
+            return false;
+        }
+
+        currentFocus = -1;
+
+        //create div element that will contain all the items
+        all_items_div = document.createElement("DIV");
+        all_items_div.setAttribute("id", this.id + "autocomplete-list");
+        all_items_div.setAttribute("class", "autocomplete-items");
+
+        //append this div elemetn as a child of the input field
+        this.parentNode.appendChild(all_items_div);
+
+        //iterate through the array of cities
+        for (i = 0; i < cities.length; i++) {
+
+            //check if the item's first letter matches with the text field value first letter
+            if (cities[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) 
+            {
+                //create div element for each maching item
+                single_item_div = document.createElement("DIV");
+
+                //make the matching letters bold
+                single_item_div.innerHTML = "<strong>" + cities[i].substr(0, val.length) + "</strong>";
+                //the rest of the letters are normal
+                single_item_div.innerHTML += cities[i].substr(val.length); 
+                
+                //add input field that will hold the current items's value (the name of the city)
+                single_item_div.innerHTML += "<input type='hidden' value='" + cities[i] + "'>";
+                
+                //close all autocomplete items (matching items) if someone click on the current item's value
+                single_item_div.addEventListener("click", function(e) {
+                    //insert the clicked item's value in the input text field
+                    input.value = this.getElementsByTagName("input")[0].value;
+
+                    //then close the list of all other autocomplete values (matching values)
+                    closeAllLists();
+                });
+
+                //add the individual item's div element as a child of the autocomplete container
+                all_items_div.appendChild(single_item_div);
+            }
+        }
+    });
+
+    //executed when a key is pressed
+    input.addEventListener("keydown", function(pressed_key) 
+    {
+        var items = document.getElementById(this.id + "autocomplete-list");
+        if (items) items = items.getElementsByTagName("div");
+
+        //if the arrow DOWN is pressed
+        if (pressed_key.keyCode == 40) 
+        {
+            //increase the currentFocus 
+            //and make the current item active
+            currentFocus++;
+            addActive(items);
+        }
+        //if the arrow UP is pressed
+        else if (pressed_key.keyCode == 38) 
+        {
+            //decrease the currentFocus 
+            //and make the current item active
+            currentFocus--;
+            addActive(items);
+        } 
+        //if the ENTER key is pressed, prevent the form from being submitted
+        else if (pressed_key.keyCode == 13) 
+        {
+            pressed_key.preventDefault();
+            if (currentFocus > -1) 
+            {
+                //simulate click on the current "active" item
+                if (items) items[currentFocus].click();
+            }
+        }
+    });
+
+    //classify and item as "active"
+    function addActive(items) 
+    {
+        if (!items) return false; //no active items
+
+        //remove the "active" class from all items
+        removeActive(items);
+
+        if (currentFocus >= items.length) currentFocus = 0;
+        if (currentFocus < 0) currentFocus = (items.length - 1);
+
+        //adding the "autocomplete-active" class
+        items[currentFocus].classList.add("autocomplete-active");
+    }
+
+    //remove the "active" classe from all autocomplete items;
+    function removeActive(autocomplete_active_items)
+    {
+        for (var i = 0; i < autocomplete_active_items.length; i++) 
+        {
+            autocomplete_active_items[i].classList.remove("autocomplete-active");
+        }
+    }
+
+    //close all autocomplete lists
+    //except the one passed as an argument
+    function closeAllLists(element) 
+    {
+        var items = document.getElementsByClassName("autocomplete-items");
+        for (var i = 0; i < items.length; i++) {
+            if (element != items[i] && element != input) {
+                items[i].parentNode.removeChild(items[i]);
+            }
+        }
+    }
+
+    /*execute a function when someone clicks in the document:*/
+    document.addEventListener("click", function (e) 
+    {
+        closeAllLists(e.target);
+    });
+}
+
+autocomplete(document.getElementById("myAuthor"), formatedDataAuthors);
+autocomplete(document.getElementById("myBook"), formatedDataBooks);
