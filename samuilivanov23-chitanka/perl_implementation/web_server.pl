d4a6adc9658e296cabb1912ed96d5c1639042043
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-11-03 15:02:44 +0200

    Perl server handling form data

--- samuilivanov23-chitanka/perl_implementation/web_server.pl
+++ samuilivanov23-chitanka/perl_implementation/web_server.pl
@@ -1,6 +1,8 @@
 use strict;
 use Socket;
 use IO::Socket;
+use DBI;
+require './dbconfig.pl';
 
 sub parse_form 
 {
@@ -10,16 +12,26 @@ sub parse_form
     foreach (split /&/, $data) 
     {
         my ($key, $val) = split /=/;
+        #substitute "+" with whitespace
         $val =~ s/\+/ /g;
+        #convert hex to chars
         $val =~ s/%(..)/chr(hex($1))/eg;
         $data{$key} = $val;
     }
+
     return %data; 
 }
 
-my $DOCUMENT_ROOT = "/media/samuil2001ivanov/808137c8-9dff-4126-82f4-006ab928a3fc1/training-projects/samuilivanov23-chitanka/perl_implementation";
-#print $DOCUMENT_ROOT."\n";
+#db config data
+our ($dbname);
+our ($dbhost);
+our ($dbusername);
+our ($dbpassword);
+our ($dbport);
 
+my $DOCUMENT_ROOT = "/media/samuil2001ivanov/808137c8-9dff-4126-82f4-006ab928a3fc1/training-projects/samuilivanov23-chitanka/perl_implementation";
+
+#create server
 my $server = new IO::Socket::INET->new (
     LocalHost => '',
     LocalPort => "1024",
@@ -38,33 +50,28 @@ while (my $client = $server->accept()) {
     {
         #Read the request
         local $/ = Socket::CRLF;
+
         while (<$client>) 
         {
-            chomp; # Get the main http request
+            chomp; # Get the main http request            
+            print "$_\n";
+
+            #split the http request
             if (/\s*(\w+)\s*([^\s]+)\s*HTTP\/(\d.\d)/) 
             {
                 $request{METHOD} = uc $1;
                 $request{URL} = $2;
                 $request{HTTP_VERSION} = $3;
-            } 
+            }
             # Standard headers
             elsif (/:/) 
             {
                 (my $type, my $val) = split /:/, $_, 2;
-                
-                $type =~ s/^\s+//;
-                foreach ($type, $val) 
-                {
-                    s/^\s+//;
-                    s/\s+$//;
-                }
-                
                 $request{lc $type} = $val;
-            } # POST data
+            } 
+            # check if end of string/line is reached -> '$'
             elsif (/^$/) 
             {
-                read($client, $request{CONTENT}, $request{'content-length'})
-                    if defined $request{'content-length'};
                 last;
             }
         }
@@ -74,59 +81,64 @@ while (my $client = $server->accept()) {
 
     if ($request{METHOD} eq 'GET') 
     {
+        print "URL: " . $request{URL} . "\n";
+        #(.*) matches any character except line break
         if ($request{URL} =~ /(.*)\?(.*)/) 
         {
-                $request{URL} = $1;
-                $request{CONTENT} = $2;
-                %data = parse_form($request{CONTENT});
-        } 
-        else 
+            $request{URL} = $1;
+            $request{CONTENT} = $2;
+            %data = parse_form($request{CONTENT});
+
+            #connect to database
+            my $database_connection = DBI -> connect("dbi:Pg:dbname=$dbname;host=$dbhost;port=$dbport",  
+                                                    $dbusername,
+                                                    $dbpassword,
+                                                    {AutoCommit => 0, RaiseError => 1}
+                                                ) or die $DBI::errstr;
+
+        }
+        else
         {
-                %data = ();
+            %data = ();
         }
         $data{"_method"} = "GET";
-    } 
-    elsif ($request{METHOD} eq 'POST') 
-    {
-                %data = parse_form($request{CONTENT});
-                $data{"_method"} = "POST";
-    } 
+    }
     else 
     {
         $data{"_method"} = "ERROR";
     }
 
-        #Serve file
-        my $endpoint = $DOCUMENT_ROOT . "/front_end/index.html";
-        print "endpoint: " . $endpoint . "\n";
+    #Serve file
+    my $endpoint = $DOCUMENT_ROOT . "/front_end/index.html";
+    print "endpoint: " . $endpoint . "\n";
 
-        # Send response
-        if (open(FILE, "$endpoint")) 
+    # Send response
+    if (open(FILE, "$endpoint")) 
+    {
+        print $client "HTTP/1.0 200 OK", Socket::CRLF;
+        print $client "Content-type: text/html", Socket::CRLF;
+        print $client Socket::CRLF;
+        my $buffer;
+        while (read(FILE, $buffer, 8192)) 
         {
-            print $client "HTTP/1.0 200 OK", Socket::CRLF;
-            print $client "Content-type: text/html", Socket::CRLF;
-            print $client Socket::CRLF;
-            my $buffer;
-            while (read(FILE, $buffer, 8192)) 
-            {
-                print $client $buffer;
-            }
-            $data{"_status"} = "200";
+            print $client $buffer;
         }
-        else
-        {
-            print $client "HTTP/1.0 404 Not Found", Socket::CRLF;
-            print $client Socket::CRLF;
-            print $client "<html><body>404 Not Found</body></html>";
-            $data{"_status"} = "404";
-        }
-        close(FILE);
+        $data{"_status"} = "200";
+    }
+    else
+    {
+        print $client "HTTP/1.0 404 Not Found", Socket::CRLF;
+        print $client Socket::CRLF;
+        print $client "<html><body>404 Not Found</body></html>";
+        $data{"_status"} = "404";
+    }
+    close(FILE);
 
-        # Log request
-        foreach (keys(%data)) 
-        {
-                print ("   $_ = $data{$_}\n"); 
-        }
+    # Log request
+    foreach (keys(%data)) 
+    {
+            print ("   $_ = $data{$_}\n"); 
+    }
 
     #close connection
     close $client;
