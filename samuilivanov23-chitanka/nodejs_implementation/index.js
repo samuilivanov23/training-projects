cab96cade309e64edf6be39c7497714694a730e8
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-11-06 18:49:46 +0200

    Changing sql query for (0-5, 5-10...) ranges to use the existing words_count data in the sentences table

--- samuilivanov23-chitanka/nodejs_implementation/index.js
+++ samuilivanov23-chitanka/nodejs_implementation/index.js
@@ -93,7 +93,7 @@ getTopSentences = function (query, parameters, response, book_name)
         }
         else
         {                        
-            if(res['rows'][0]['sum'] == null)
+            if(res['rows'][0]['count'] == null)
             {
                 console.log("Author-Book does not match!")
                 getTop10Authors(response)
@@ -121,7 +121,7 @@ sentencesRanges = function (resultSentences, response, book_name)
 
     resultSentences.forEach(sentences => {
         x_axis_items.push(ranges_map[sentences['range']])
-        y_axis_items.push(sentences['sum'])
+        y_axis_items.push(sentences['count'])
     })
 
     let authors_autocomplete = getAllAuthors()
@@ -203,12 +203,12 @@ app.get('/plot', function(request, response) {
     let query = ""
     let parameters = []
 
-    let first_start = '0';
-    let first_end = '5';
-    let second_end = '10';
-    let third_end = '15';
-    let fourth_end = '20';
-    let fifth_end = '70';
+    // let first_start = '0';
+    // let first_end = '5';
+    // let second_end = '10';
+    // let third_end = '15';
+    // let fourth_end = '20';
+    // let fifth_end = '70';
 
     if(author_name != "" && book_name != "")
     {
@@ -224,51 +224,67 @@ app.get('/plot', function(request, response) {
             {
                 let author_id = res['rows'][0]['id']
 
-                query =`select '1' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on
-                        b.id=s.book_id where b.author_id=$1 and b.name=$2 and s.words_count>=$3 and s.words_count<$4 group by s.words_count) as a 
-                        union
-                        select '2' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.author_id=$5 and b.name=$6 and s.words_count>=$7 and s.words_count<$8 group by s.words_count) as a
-                        union
-                        select '3' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.author_id=$9 and b.name=$10 and s.words_count>=$11 and s.words_count<$12 group by s.words_count) as a
-                        union
-                        select '4' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.author_id=$13 and b.name=$14 and s.words_count>=$15 and s.words_count<$16 group by s.words_count) as a                                                                   
-                        union
-                        select '5' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.author_id=$17 and b.name=$18 and s.words_count>=$19 and s.words_count<$20 group by s.words_count) as a
-                        order by range asc`
-            
-                parameters = [author_id, book_name, first_start, first_end, 
-                              author_id, book_name, first_end, second_end, 
-                              author_id, book_name, second_end, third_end, 
-                              author_id, book_name, third_end, fourth_end, 
-                              author_id, book_name, fourth_end, fifth_end]
+                query = `select id from books where name=$1 and author_id=$2`
+                parameters = [book_name, author_id]
 
-                getTopSentences(query, parameters, response, book_name)
+                client.query(query, parameters, (err, res) => {
+                    if(err != null)
+                    {
+                        console.log(err)
+                    }
+                    else
+                    {
+                        let book_id = res['rows'][0]['id']
+
+                        query =`select '1' as range, count(id) from sentences where book_id=$1 and words_count>=0 and words_count<5
+                                union
+                                select '2' as range, count(id) from sentences where book_id=$2 and words_count>=5 and words_count<10
+                                union
+                                select '3' as range, count(id) from sentences where book_id=$3 and words_count>=10 and words_count<15
+                                union
+                                select '4' as range, count(id) from sentences where book_id=$4 and words_count>=15 and words_count<20
+                                union
+                                select '5' as range, count(id) from sentences where book_id=$5 and words_count>=20 and words_count<70
+                                order by range asc`
+            
+                        parameters = [book_id, book_id, book_id, book_id, book_id,]
+
+                        getTopSentences(query, parameters, response, book_name)
+                    }
+                })
             }
         })
     }
     else if (book_name != "")
     {
+        query = `select id from books where name=$1`
+        parameters = [book_name]
 
-        query = `select '1' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on
-            b.id=s.book_id where b.name=$1 and s.words_count>=$2 and s.words_count<$3 group by s.words_count) as a 
-            union
-            select '2' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$4 and s.words_count>=$5 and s.words_count<$6 group by s.words_count) as a
-            union
-            select '3' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$7 and s.words_count>=$8 and s.words_count<$9 group by s.words_count) as a
-            union
-            select '4' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$10 and s.words_count>=$11 and s.words_count<$12 group by s.words_count) as a                                                                   
-            union
-            select '5' as range, sum(sentences_count) from (select s.words_count, count(s.sentence) as sentences_count from books as b join sentences as s on b.id=s.book_id where b.name=$13 and s.words_count>=$14 and s.words_count<$15 group by s.words_count) as a
-            order by range asc`
-        
-        let parameters = [book_name, first_start, first_end,
-                          book_name, first_end, second_end,
-                          book_name, second_end, third_end,
-                          book_name, third_end, fourth_end,
-                          book_name, fourth_end, fifth_end]
+        client.query(query, parameters, (err, res) => {
+            if(err != null)
+            {
+                console.log(err)
+            }
+            else
+            {
+                let book_id = res['rows'][0]['id']
 
+                query =`select '1' as range, count(id) from sentences where book_id=$1 and words_count>=0 and words_count<5
+                        union
+                        select '2' as range, count(id) from sentences where book_id=$2 and words_count>=5 and words_count<10
+                        union
+                        select '3' as range, count(id) from sentences where book_id=$3 and words_count>=10 and words_count<15
+                        union
+                        select '4' as range, count(id) from sentences where book_id=$4 and words_count>=15 and words_count<20
+                        union
+                        select '5' as range, count(id) from sentences where book_id=$5 and words_count>=20 and words_count<70
+                        order by range asc`
         
-        getTopSentences(query, parameters, response, book_name)
+                let parameters = [book_id, book_id, book_id, book_id, book_id]
+
+                getTopSentences(query, parameters, response, book_name)
+            }
+        })
     }
     else if(author_name != "")
     {
