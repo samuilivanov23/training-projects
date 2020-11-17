6299cf21eeef8b808d3c0d619d1e264325c390d5
samuilivanov23 <samuil2001ivanov@gmail.com>
2020-11-17 09:51:09 +0200

    Adding count select option to for adding products to cart.

--- samuilivanov23-internet_shop/frontend/src/Components/ProductDetails.js
+++ samuilivanov23-internet_shop/frontend/src/Components/ProductDetails.js
@@ -10,7 +10,56 @@ class ProductDetails extends React.Component {
         product: PropTypes.object.isRequired,
     };
 
+    constructor(props){
+        super(props);
+
+        this.state = {
+            in_stock : '',
+            selectedCount : 0,
+        }
+    }
+
+    checkProductInStock(product){
+        if(product['count'] > 0){
+            this.setState({
+                in_stock : 'In stock'
+            });
+        }
+        else{
+            this.setState({
+                in_stock : 'Out of stock'
+            });
+        }
+    }
+
+    generateCountSelectElements(product_count){
+        const options = [];
+
+        for(let i = 1; i <= product_count; i++){
+            options.push(<option key={i} value={i}>{i}</option>);
+        }
+
+        return options;
+    }
+
+    componentDidMount(){
+        this.checkProductInStock(this.props.product);
+
+    }
+
+    changeProductSelectCount = (event) => {
+        console.log('current product count');
+        console.log(event.target.value);
+
+        this.setState({ 
+            selectedCount: event.target.value
+        });
+    }
+
     render(){
+        const options = this.generateCountSelectElements(this.props.product['count']);
+        console.log(options);
+
         return (
             <div>
                 <Card style={{ width: '36rem' }}>
@@ -18,9 +67,17 @@ class ProductDetails extends React.Component {
                         <Card.Title>Name: {this.props.product['name']}</Card.Title>
                         <Card.Text>Description: {this.props.product['description']}</Card.Text>
                         <Card.Text>Price: {this.props.product['price']} lv.</Card.Text>
-                        <Card.Text>In stock: //TODO</Card.Text>
+                        <Card.Text>{this.state.in_stock}</Card.Text>
+                        
+                        <select name={'selected_count'} value={this.state.selectedCount} onChange={this.changeProductSelectCount}>
+                            {options}
+                        </select>
+                        
+                        <Button onClick={() => this.addProductToCart(this.props.product)}>
+                            Add to cart
+                        </Button>
                     </Card.Body>
-                </Card>            
+                </Card>
             </div>
         );
     }
