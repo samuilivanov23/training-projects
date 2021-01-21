b755db6ae1aabd850ef967b4447a0dccbe086fd7
samuilivanov23 <samuil2001ivanov@gmail.com>
2021-01-20 16:06:27 -0800

    Comments and post filtering based on @mentions

--- samuilivanov23-GoogleDrive_comments/comments.py
+++ samuilivanov23-GoogleDrive_comments/comments.py
@@ -6,9 +6,9 @@ email_regex = '[\w\.-]+@[\w\.-]+'
 my_emails = ["samuil.iv@arc-global.com", "samuil.iv@hackerschool-bg.com"]
 
 #testing email_regex
-# print(re.match(email_regex, "samuil.iv@hackerschool-bg.com"))
-# print(re.match(email_regex, "samuil.iv@arc-global.com"))
-# print(re.match(email_regex, "samuil2001ivanov@gmail.com"))
+# print(re.findall(email_regex, "@samuil.iv@hackerschool-bg.com problem na 10ti red"))
+# print(re.findall(email_regex, "@samuil.iv@arc-global.com problem na 15ti red"))
+# print(re.findall(email_regex, "@samuil2001ivanov@gmail.com problem na 100tni red"))
 
 
 CLIENT_SECRET_FILE = "credentials.json"
@@ -16,13 +16,13 @@ API_NAME = "drive"
 API_VERSION = "v3"
 SCOPES = ["https://www.googleapis.com/auth/drive"]
 FILE_ID = "1NbcJAgaVwqUtmuZbyBHgeN6PyaWJGxH6LNHvxCyipT0"
-#FILE_ID = "1WPS3EYEYkQpMJ9mDCiZoKw6uU5CYTDLMQvAQhbbSkAU"
+FILE_NAME = "DriveAPI comments testing"
 
 if __name__ == "__main__":
+    data_to_send = []
     service = CreateService(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
-
     results = service.files().list(
-        pageSize=10, fields='nextPageToken, files(id, name, createdTime, webViewLink)').execute()
+        q="name='%s'" % FILE_NAME, pageSize=10, fields='nextPageToken, files(id, name, createdTime, webViewLink)').execute()
 
     files = results.get('files', [])
 
@@ -33,43 +33,50 @@ if __name__ == "__main__":
         for file in files:
             print(file)
 
-    print("\n\n")
+            results = service.comments().list(fileId=file['id'], fields="comments(author, content, replies, createdTime)").execute()
+            comments = results.get('comments', [])
 
-    results = service.comments().list(fileId=FILE_ID, fields="comments(author, content, replies, createdTime)").execute()
-    comments = results.get('comments', [])
+            if not comments:
+                print("HEREEE!?!?!?????")
+                data_to_send.append({'fileName': file['name'], 'msg' : "No comments in that file"})
+            else:
+                for comment in comments:
+                    if comment['replies']:
+                        for reply in reversed(comment['replies']):
+                            print(reply['content'])
+                            emails_found = re.findall(email_regex, reply['content'])
 
-    if not comments:
-        print("No comments found")
-    else:
-        print("Comments:")
-        for comment in comments:
-            print('NEW POST')
-            print(comment['author']['displayName'])
-            print(comment['content'])
-            
-            if comment['replies']:
-                #TODO check the last reply I'm tagged in
-                for reply in reversed(comment['replies']):
-                    #TODO check if I'm tagged and break if that's the case
-                    print(reply['author']['displayName'])
-                    print(reply['content'])
-                    print("----------------------------------------------------------------------")
-
-                    try:
-                        emails_found = re.findall(email_regex, reply['content'])
-                        if emails_found:
-                            print(emails_found)
-                            if (my_emails[0] in emails_found or my_emails[1] in emails_found):
-                                print("Send data to Slack")
+                            if emails_found and (my_emails[0] in emails_found or my_emails[1] in emails_found):
+                                data_to_send.append({
+                                    'author' : reply['author']['displayName'],
+                                    'content' : reply['content'],
+                                    'timestamp' : reply['createdTime'],
+                                    'webViewLink' : file['webViewLink']
+                                })
+                                break
                             else:
                                 print("Don't send data to Slack")
-                            break
+                    else:
+                        print("here")
+                        emails_found = re.findall(email_regex, comment['content'])
+
+                        if (not comment['author']['displayName'] == "My name here") and emails_found and (my_emails[0] in emails_found or my_emails[1] in emails_found):
+                            data_to_send.append({
+                                'author' : comment['author']['displayName'],
+                                'content' : comment['content'],
+                                'timestamp' : comment['createdTime'],
+                                'webViewLink' : file['webViewLink']
+                            })
+                            print("Send data to Slack")
                         else:
-                            print("Not tagged in reply")
-                    except Exception as e:
-                        print(e)
-                        
-                print("\n\n")
-            else:
-                #TODO check if I'm tagged in the thost
-                print("No replies to this post")
+                            print("Don't send data to Slack")
+                            
+
+print("\n")
+for data in data_to_send:
+    print("author: " + data['author'])
+    print("content: " + data['content'])
+    print("timestamp: " + data['timestamp'])
+    print("webViewLink: " + data['webViewLink'])
+
+    print("\n---------------------------------------\n")
